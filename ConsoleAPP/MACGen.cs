b244ffcc3a13535433fb25dcc9b59c987840842c
LeftBased <59938062+LeftBased@users.noreply.github.com>
2020-01-15 13:20:09 -0800

    Add files via upload

---
+++ ConsoleAPP/MACGen.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading; //individual thread!
+using System.Diagnostics; //for our stopwatch
+using C5; //C5 Random [XorShift] by Niels Kokholm, Peter Sestoft, and Rasmus Lystrøm (https://github.com/sestoft/C5) under MIT License.
+
+namespace MiniMACGen
+{
+    class MACGen
+    {
+        public static readonly C5Random getRandom2 = new C5Random();
+        public static string aT = string.Empty;
+        public static string myOptionsRet = string.Empty;
+        public static string numOUI = string.Empty;
+
+        public string Options(int Select, string inputOUI)
+        {
+
+            numOUI = inputOUI;
+
+            switch (Select)
+            {
+                case 0:
+                    myOptionsRet = GenerateHex(":", true, string.Empty);
+                    break;
+                case 1:
+                    myOptionsRet = GenerateHex("-", false, numOUI);
+                    break;
+                case 2:
+                    myOptionsRet = GenerateHex(string.Empty, true, string.Empty);
+                    break;
+                case 3:
+                    myOptionsRet = GenerateHex(string.Empty, false, numOUI);
+                    break;
+                case 4:
+                    myOptionsRet = GenerateHex(string.Empty, false, numOUI);
+                    break;
+                case 5:
+                    myOptionsRet = GenerateHex(" ", false, numOUI);
+                    break;
+                case 6:
+                    myOptionsRet = GenerateHex(" ", true, string.Empty);
+                    break;
+                case 7:
+                    myOptionsRet = GenerateHex("-", false, numOUI);
+                    break;
+                case 8:
+                    myOptionsRet = GenerateHex(" ", true, numOUI);
+                    break;
+            }
+            return myOptionsRet;
+        }
+        public string RandomHexStr()
+        {
+            C5Random rdm = getRandom2;
+            string hexValue = string.Empty;
+            int num;
+            num = rdm.Next(8, 101);
+            hexValue = num.ToString("X2");
+            return hexValue;
+        }
+
+        public string GenerateHex(string mySymbol, bool my00s, string myOUI)
+        {
+            string aT = string.Empty;
+
+            if (my00s)
+            {
+                aT = "00" + mySymbol + RandomHexStr() + mySymbol + RandomHexStr() + mySymbol + RandomHexStr() + mySymbol + RandomHexStr() + mySymbol + RandomHexStr();
+            }
+            else
+            {
+                if (myOUI == "")
+                {
+                    aT = RandomHexStr() + mySymbol + RandomHexStr() + mySymbol + RandomHexStr() + mySymbol + RandomHexStr() + mySymbol + RandomHexStr() + mySymbol + RandomHexStr();
+                }
+                else
+                {
+                    string[] mySplitter = myOUI.Split(':');
+                    aT = mySplitter[0].ToUpper() + mySymbol + mySplitter[1].ToUpper() + mySymbol + mySplitter[2].ToUpper() + mySymbol + RandomHexStr() + mySymbol + RandomHexStr() + mySymbol + RandomHexStr();
+                }
+            }
+            return aT;
+        }
+
+    }
+}
---
+++ ConsoleAPP/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniMACGen
+{
+    class Program
+    {
+        public static int iCount; //we need this for the loops
+        public static int iCu; //we need this for max loop
+        public static string MyBigMAC = string.Empty; //store genned mac temporarily.
+
+        static void Main(string[] args)
+        {
+            MACGen MyGen = new MACGen();// -- call our new class!
+            List<string> GenMACs = new List<string>(); //This is our list.
+
+            int ABC = 100; //we need to count.
+            string DFG, HIJ, KLM, NOP, TUV;
+            bool QRS = false;
+
+            Console.Title = "MAC Address Generator v0.2";
+            Console.WriteLine("Welcome. Please enter how many MAC Addresses you want:");
+            TUV = Console.ReadLine();
+            if (TUV == ""|| TUV == null)
+            {
+                Console.WriteLine("[ERROR] Please enter a valid number:");
+                TUV = Console.ReadLine();
+             
+                if (TUV == "" || TUV == null)
+                {
+                    Console.WriteLine("Since you didn't put a valid number\nWe've set default to 100.");
+                    ABC = 100;
+                } else
+                {
+                    ABC = Convert.ToInt32(TUV);
+                    Console.WriteLine("You've entered" + ABC);
+                }
+            }
+            Console.WriteLine("Do you want seperators: y/n?");
+            NOP = Console.ReadLine();
+            if (NOP == "y")
+            {
+                QRS = true;
+            }
+            else
+            {
+                QRS = false;
+            }
+       
+            Console.WriteLine($"Thank you. Generating #: {ABC}");
+            iCu = ABC; //set our count.
+            if (QRS)
+            {
+                GenMACs.Clear(); // clear list just incase.
+                for (iCount = 0; iCount < iCu; iCount++)
+                {
+                    GenMACs.Add(MyGen.Options(0, ""));
+                }
+            }
+            else
+            {
+                GenMACs.Clear(); // clear list just incase.
+                for (iCount = 0; iCount < iCu; iCount++)
+                {
+                    GenMACs.Add(MyGen.Options(2, ""));
+                }
+            }
+            Console.WriteLine("Do you want us to see generated results? y/n");
+            DFG = Console.ReadLine();
+            if (DFG == "y")
+            {
+                Print(GenMACs);
+            }
+            Console.WriteLine("Do you wish to save to a text file? y/n");
+            HIJ = Console.ReadLine();
+            if (HIJ == "y")
+            {
+                Console.WriteLine("Please enter a filename:");
+                KLM = Console.ReadLine();
+                if (KLM == "")
+                {
+                    Console.WriteLine("[ERROR] Please enter a filename:");
+                    KLM = Console.ReadLine();
+                }
+                System.IO.File.WriteAllLines(KLM + ".txt", GenMACs);
+                Console.WriteLine($"Saved to: {KLM}.txt");
+            }
+            Console.WriteLine("Thank you for using MAC Generator v0.2\nPlease press any key to exit");
+            Console.ReadLine();
+
+        }
+
+        static void Print(IList<string> list)
+        {
+            Console.WriteLine("Count: {0}", list.Count);
+            foreach (string value in list)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+
+}
